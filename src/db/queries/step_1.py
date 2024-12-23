4b0cafe39def8c6bec4e5bc21d2525dea4f8f173
Denilson Oliveira <juniod04@gmail.com>
2024-12-23 14:27:49 -0300

    feat: add query for classification step 1

--- src/db/queries/step_1.py
+++ src/db/queries/step_1.py
@@ -1,15 +1,43 @@
-from config import cursor as cur
-from config import con
+from src.db.config import ConfigDB
+
+class UpdatedNegativeWords:
+    query = """
+    UPDATE "{schema_name}"."{table_name}" 
+    SET classificacao = true, fase = 1 
+    where descricao LIKE %s"""
+    def __init__(self, schema_name, table_name):
+        self.schema_name = schema_name
+        self.table_name = table_name
+        
+   
+    def execute(self,cursor):
+        query_formated = self.query.format(schema_name = self.schema_name, table_name = self.table_name)
+        
+        negative_words = [
+            'Office', 'Office 365', 'SQL', 'Windows', 'Adobe', 'Autocad', 'CorelDraw', 'Power', 'VMware', 'vSphere', 
+            'Copilot', 'Autodesk', 'Acrobat', 'Revit', 'Acquia', 'ARCHITECTURE', 'DYNAMICS', 'Creative', 'E-Mobility',
+            'BPO'  
+            ]
+        
+        for word in negative_words:
+            cursor.execute(query_formated, (f'%{word}%',))
+            print(cursor.statusmessage)
 
 
 
+
+
+
+        
+
+
+    
 
 
 
 
 
   
-
 
 
 
