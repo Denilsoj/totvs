1dc788d0bf0a5b1cc9254688e293a22678a6f188
Lucashcr <lucash.rocha@hotmail.com>
2024-12-23 16:02:21 -0300

    fix: Format and improve code legibility

--- src/db/config.py
+++ src/db/config.py
@@ -1,7 +1,5 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from psycopg2 import errors
-from psycopg2 import sql
 import os
 from dotenv import load_dotenv
 
@@ -11,125 +9,38 @@ load_dotenv()
 class ConfigDB:
 
     def __init__(self):
-        self.con = psycopg2.connect(
-            dbname = os.getenv('NAME_DB'), 
-            user = os.getenv('USER_DB'), 
-            password = os.getenv('PASSWORD_DB'),
-            host = os.getenv('HOST_DB'),
-            port = os.getenv('PORT_DB')
-            
-            )
+        self.connection = psycopg2.connect(
+            dbname=os.getenv("NAME_DB"),
+            user=os.getenv("USER_DB"),
+            password=os.getenv("PASSWORD_DB"),
+            host=os.getenv("HOST_DB"),
+            port=os.getenv("PORT_DB"),
+        )
 
-        self.cursor = self.con.cursor(cursor_factory=RealDictCursor)
+        self.cursor = self.connection.cursor(cursor_factory=RealDictCursor)
 
-    @staticmethod
-    def duplicate_table(schema, table_name, dump_table, connection, cursor):
+    def duplicate_table(self, schema, table_name, dump_table):
 
-        with connection:
-            try:
-                queries = [
-                    f'CREATE TABLE IF NOT EXISTS "{schema}"."{table_name}" AS SELECT * FROM "{schema}"."{dump_table}"',
-                    f'UPDATE "{schema}"."{table_name}" SET classificacao = false',
-                    f'UPDATE "{schema}"."{table_name}" SET modalidade = \'Outras modalidades\' WHERE modalidade = \'0\'',
-                    f'UPDATE "{schema}"."{table_name}" SET modalidade = \'Pregão eletrônico\' WHERE modalidade = \'5\'',
-                    f'UPDATE "{schema}"."{table_name}" SET modalidade = \'Dispensa de licitação\' WHERE modalidade = \'19\'',
-                    f'UPDATE "{schema}"."{table_name}" SET modalidade = \'Pregão presencial\' WHERE modalidade = \'6\'',
-                    f'UPDATE "{schema}"."{table_name}" SET modalidade = \'Inexigibilidade\' WHERE modalidade = \'28\''
-                    ]
+        try:
+            queries = [
+                f'CREATE TABLE IF NOT EXISTS "{schema}"."{table_name}" AS SELECT * FROM "{schema}"."{dump_table}"',
+                f'UPDATE "{schema}"."{table_name}" SET classificado = false',
+                f"UPDATE \"{schema}\".\"{table_name}\" SET modalidade = 'Outras modalidades' WHERE modalidade = '0'",
+                f"UPDATE \"{schema}\".\"{table_name}\" SET modalidade = 'Pregão eletrônico' WHERE modalidade = '5'",
+                f"UPDATE \"{schema}\".\"{table_name}\" SET modalidade = 'Dispensa de licitação' WHERE modalidade = '19'",
+                f"UPDATE \"{schema}\".\"{table_name}\" SET modalidade = 'Pregão presencial' WHERE modalidade = '6'",
+                f"UPDATE \"{schema}\".\"{table_name}\" SET modalidade = 'Inexigibilidade' WHERE modalidade = '28'",
+            ]
 
+            for query in queries:
+                self.cursor.execute(query)
 
+                self.connection.commit()
+                print(self.cursor.statusmessage)
 
-                for query in queries:
-                    cursor.execute(query)
-        
+        except Exception as e:
+            print(e)
 
-                    connection.commit()
-                    print(cursor.statusmessage)
-
-            except Exception as e:
-                print(e)
-
-
-           
-
-
-
-    
-
-# def create_table(schema:str ,table_name:str) -> str:
-#     try:
-#         create_table_query = sql.SQL("""
-#         CREATE TABLE IF NOT EXISTS {schema}.{table_name} (
-#             id SERIAL PRIMARY KEY,
-#             item_id VARCHAR(50) UNIQUE,
-#             data DATE,
-#             orgao TEXT,
-#             uf VARCHAR(2),
-#             objeto TEXT,
-#             cnpj VARCHAR(30),
-#             razao_social TEXT,
-#             marca TEXT,
-#             modelo TEXT,
-#             fabricante TEXT,
-#             unidade_de_medida TEXT,
-#             modalidade TEXT,
-#             valor_total NUMERIC(32, 4),
-#             valor_unitario NUMERIC(32, 4),
-#             base TEXT,
-#             descricao TEXT,
-#             termo TEXT,
-#             classificacao BOOL,
-#             fase INT
-#         )
-#     """).format(
-#         schema=sql.Identifier(schema),
-#         table_name=sql.Identifier(table_name)
-#     )
-
-
-#         cur.execute(create_table_query)
-        
-
-#         print(cur.statusmessage)
-#     except errors.Error as e:
-#         print(f"Erro ao criar a tabela {table_name}", e)
-#     finally:
-#         con.commit()
-#         cur.close()
-#         con.close()
-
-# def insert_data_table(schema,table_name, **d):
-   
-#     print(d)
-#     with con:
-#         cur = con.cursor()
-#         try:
-#                 query = sql.SQL(
-#                      """
-#                 INSERT INTO {schema}.{table_name} (
-#                     item_id, data, orgao, uf, objeto,
-#                     cnpj, razao_social, marca, modelo, fabricante,
-#                     unidade_de_medida, modalidade, valor_total, valor_unitario, base,
-#                     descricao, termo
-#                 ) VALUES (
-#                     %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 
-#                     %s, %s, %s, %s, %s, %s, %s
-#                 )
-#                 """
-#                 ).format(
-#                     schema=sql.Identifier(schema),
-#                     table_name=sql.Identifier(table_name)
-#                 )
-            
-#                 cur.execute(query, (
-#                         str(d['item_id']), d['data'], d['orgao'], d['uf'], d['objeto'],
-#                         d['cnpj'], d['razao_social'], d['marca'], d['modelo'], d['fabricante'],
-#                         d['unidade_de_medida'], d['modalidade'], round(float(d['valor'].replace('.', '').replace(',', '.').strip()), 4) if len(d['valor']) != 0 else d['valor'],round(float(d['valor_unitario'].replace('.', '').replace(',', '.').strip()), 4) if len(d['valor_unitario']) != 0 else d['valor_unitario'], d['base'],
-#                         d['descricao'], d['termo']
-#                     ))
-                
-#                 con.commit()
-               
-#         except errors.Error as e:
-            
-#             print('error ao adiconar', e)
+    def __del__(self):
+        self.cursor.close()
+        self.connection.close()
--- src/db/queries/step_1.py
+++ src/db/queries/step_1.py
@@ -1,80 +1,52 @@
-from src.db.config import ConfigDB
+from psycopg2 import sql
+
 
 class UpdatedNegativeWords:
-    query = """
-    UPDATE "{schema_name}"."{table_name}" 
-    SET classificacao = true, fase = 1 
-    WHERE  conditions"""
+    query = sql.SQL(
+        """
+        UPDATE "{schema_name}"."{table_name}" 
+        SET classificado = true, fase = 1 
+        WHERE {conditions};
+        """
+    )
+
     def __init__(self, schema_name, table_name):
         self.schema_name = schema_name
         self.table_name = table_name
-        
-   
+
     def execute(self, cursor):
-        query_formated = self.query.format(schema_name = self.schema_name, table_name = self.table_name)
-
         negative_words = [
-        'Office', 
-        'Office 365', 
-        'SQL', 
-        'Windows', 
-        'Adobe', 
-        'Autocad', 
-        'CorelDraw',
-        'Power', 
-        'VMware', 
-        'vSphere', 
-        'Copilot', 
-        'Autodesk', 
-        'Acrobat', 
-        'Revit', 
-        'Acquia', 
-        'ARCHITECTURE', 
-        'DYNAMICS', 
-        'Creative', 
-        'E-Mobility',
-        'BPO'  
+            "Office",
+            "Office 365",
+            "SQL",
+            "Windows",
+            "Adobe",
+            "Autocad",
+            "CorelDraw",
+            "Power",
+            "VMware",
+            "vSphere",
+            "Copilot",
+            "Autodesk",
+            "Acrobat",
+            "Revit",
+            "Acquia",
+            "ARCHITECTURE",
+            "DYNAMICS",
+            "Creative",
+            "E-Mobility",
+            "BPO",
         ]
-      
-    
-        conditions = " OR ".join([f"descricao ILIKE %s" for _ in negative_words])
-        
-        final_query = query_formated.replace("conditions", conditions)
 
-    
-        params = [f'%{word}%' for word in negative_words]
-       
-        
-        cursor.execute(final_query, params)
+        conditions = sql.Composable(
+            " OR ".join([f"descricao ILIKE {word}" for word in negative_words])
+        )
+
+        final_query = self.query.format(
+            schema_name=sql.Identifier(self.schema_name),
+            table_name=sql.Identifier(self.table_name),
+            conditions=conditions,
+        )
+
+        cursor.execute(final_query)
         print(cursor.statusmessage)
-
-
-
-
-
-
-
-
-        
-
-
-    
-
-
-
-
-
-  
-
-
-
-
-            
-
-
-
-
-
-
-
-
