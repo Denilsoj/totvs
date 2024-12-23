8620570ea91c9ad669df5070f9d59463198e7742
Denilson Oliveira <juniod04@gmail.com>
2024-12-23 14:57:02 -0300

    fix: refactor query

--- src/db/queries/step_1.py
+++ src/db/queries/step_1.py
@@ -4,24 +4,51 @@ class UpdatedNegativeWords:
     query = """
     UPDATE "{schema_name}"."{table_name}" 
     SET classificacao = true, fase = 1 
-    where descricao LIKE %s"""
+    WHERE  conditions"""
     def __init__(self, schema_name, table_name):
         self.schema_name = schema_name
         self.table_name = table_name
         
    
-    def execute(self,cursor):
+    def execute(self, cursor):
         query_formated = self.query.format(schema_name = self.schema_name, table_name = self.table_name)
-        
+
         negative_words = [
-            'Office', 'Office 365', 'SQL', 'Windows', 'Adobe', 'Autocad', 'CorelDraw', 'Power', 'VMware', 'vSphere', 
-            'Copilot', 'Autodesk', 'Acrobat', 'Revit', 'Acquia', 'ARCHITECTURE', 'DYNAMICS', 'Creative', 'E-Mobility',
-            'BPO'  
-            ]
+        'Office', 
+        'Office 365', 
+        'SQL', 
+        'Windows', 
+        'Adobe', 
+        'Autocad', 
+        'CorelDraw',
+        'Power', 
+        'VMware', 
+        'vSphere', 
+        'Copilot', 
+        'Autodesk', 
+        'Acrobat', 
+        'Revit', 
+        'Acquia', 
+        'ARCHITECTURE', 
+        'DYNAMICS', 
+        'Creative', 
+        'E-Mobility',
+        'BPO'  
+        ]
+      
+    
+        conditions = " OR ".join([f"descricao ILIKE %s" for _ in negative_words])
         
-        for word in negative_words:
-            cursor.execute(query_formated, (f'%{word}%',))
-            print(cursor.statusmessage)
+        final_query = query_formated.replace("conditions", conditions)
+
+    
+        params = [f'%{word}%' for word in negative_words]
+       
+        
+        cursor.execute(final_query, params)
+        print(cursor.statusmessage)
+
+
 
 
 
